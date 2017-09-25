6fa5e29e91411e5be9728a73e8b6a0950001e4c2
acidloop <rakivvode@yandex.ru>
2017-09-26 00:57:42 +0300

    first problem to init repo

---
+++ begin1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	double a;
+	cin >> a;
+	cout << 4*a;
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+This repository is being used for commiting solved problems from book '1000_zadach_po_programmirovaniyu';
