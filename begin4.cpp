37277df28acbbc96f9c72ad266dca10ccd7cc63e
ac1dloop <rakivvode@yandex.ru>
2017-09-30 01:58:49 +0300

    added 4 files

---
+++ begin4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	//Find length of circumstance using diameter
+	double d, Pi=3.14159265359;
+	cin >> d;
+	cout << Pi*d << endl;
+	return 0;
+}
---
+++ begin5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	//Find volume of cube and area
+	double a;
+	cin >> a;
+	cout << "Volume: " << a*a*a << " Area: " << 6*a*a << endl; 
+	return 0;
+}
