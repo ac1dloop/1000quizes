33b894dbf9228b5dcb1f9580840538ae6b297f0a
ac1dloop <rakivvode@gmail.com>
2018-10-08 16:36:06 +0300

    1

---
+++ matrix/matrix11.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	int m,n;
+	int k;
+
+	cin >> m >> n;
+
+	int arr[n][m];
+
+	for (int i=0;i<n;++i){
+		for (int j=0;j<m;++j){
+			cin >> arr[i][j];
+		}
+	}
+
+	for (int i=0;i<n;++i){
+		if (i%2==0){
+			for (int j=0;j<m;++j){
+				cout << arr[i][j] << " ";
+			}
+		} else {
+			for (int j=m-1;j>=0;--j){
+				cout << arr[i][j] << " ";
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
