a6d36c0d989246d400cbdb3af336d036e333b1fa
ac1dloop <rakivvode@gmail.com>
2018-10-24 20:00:58 +0300

    pheow

---
+++ matrix/matrix80.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	int m;
+	cin >> m;
+
+	int arr[m][m];
+	for (int i=0;i<m;++i){
+		for (int j=0;j<m;++j){
+			cin >> arr[i][j];
+		}
+	}
+	int S=0;
+
+	for (int i=0,j=0;i<m;++i,++j){
+		S+=arr[i][j];
+	}
+
+	cout << "sum: " << S << endl;
+
+	return 0;
+}
